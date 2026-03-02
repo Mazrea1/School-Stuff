bbab2723bebaf59ca879221138912321bcdef4e3
Mazreal23 <michael.lalaj@outlook.com>
2026-03-02 14:44:14 -0600

    added the code for my assignment Assignment-Inheritance

--- Spring_2026/Assignment_Inheritance/laptop_with_Attributes.java
+++ Spring_2026/Assignment_Inheritance/laptop_with_Attributes.java
@@ -1,14 +1,14 @@
 package Assignment_Inheritance;
 
 public class laptop_with_Attributes extends computer_with_Attributes {
-    public String size = "15 inches";
+    public String size = "18 inches";
     public Boolean isTouchscreen = true;
 
     laptop_with_Attributes(String memory, String storage, String processor) {
         this.memory = memory;
         this.storage = storage;
         this.processor = processor;
-        this.size = "15 inches";
+        this.size = "18 inches";
         this.isTouchscreen = true;
     }
 }
